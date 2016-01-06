5700c61c3726d138b13ace93b2bcd07616ec89b1
ajuckler <Antoine.Juckler@UGent.be>
2016-01-06 17:58:56 +0900

    Added plot at 0rad in pattern script

--- comp_patterns.m
+++ comp_patterns.m
@@ -7,7 +7,7 @@ else
 end;
 
 % Uniform
-for sizes=[32 60 64]
+for sizes=[60 64]
     % Init
     arr = AntArray(zeros(sizes), 60500, [], 0.84);
     arr = arr.setName(['fullx_' mat2str(sizes)]);
@@ -26,11 +26,13 @@ for sizes=[32 60 64]
     
     % Plots
     arr.plotAntArray();
-    arr = arr.genPattern(11000, 3000, 'XY', 30);
-    arr = arr.genPattern([], [], 'XY-BW');
-    arr = arr.E_strength(15000, 0, 0, 500);
-    arr = arr.genPattern([1 2:2:10]*1000, 3000, 'YZ', 30);
-    arr = arr.genPattern([1 2:2:10]*1000, [], 'YZ-BW');
+    arr = arr.genPattern(11000, 3000, 'theta', 30, 0);
+    arr = arr.genPattern([], [], 'theta-BW', [], 0);
+%     arr = arr.genPattern(11000, 3000, 'XY', 30);
+%     arr = arr.genPattern([], [], 'XY-BW');
+%     arr = arr.E_strength(15000, 0, 0, 500);
+%     arr = arr.genPattern([1 2:2:10]*1000, 3000, 'YZ', 30);
+%     arr = arr.genPattern([1 2:2:10]*1000, [], 'YZ-BW');
 end;
 
 % Squares
@@ -78,6 +80,8 @@ end;
 % 
 %     % Plots
 %     arr.plotAntArray();
+%     arr = arr.genPattern(11000, 3000, 'theta', 30, 0);
+%     arr = arr.genPattern([], [], 'theta-BW', [], 0);
 %     arr = arr.genPattern(11000, 3000, 'XY', 30);
 %     arr = arr.genPattern([], [], 'XY-BW');
 %     arr = arr.E_strength(15000, 0, 0, 500);
@@ -85,56 +89,58 @@ end;
 %     arr = arr.genPattern([1 2:2:10]*1000, [], 'YZ-BW');
 % end;
 
-for rem_els=8:15
-    disp(['Squares 2, iteration ' num2str(rem_els-8) ' of ' num2str(15-8)]);
-    % Init
-    arr = AntArray(zeros(64), 60500, [], 0.84);
-    arr = arr.setName(['sq2_' mat2str(rem_els)]);
-    arr = arr.setMax('XY', 30);
-    arr = arr.setMax('YZ', 30);
-    arr = arr.setMax('E', 25);
-    arr = arr.setMin('XY', -60);
-    arr = arr.setMin('YZ', -60);
-    arr = arr.setMin('E', -15);
-
-    % Create elements' pattern
-    tmp = zeros(64);
-    for i=16:31-rem_els
-        tmp(i, i:end-i+1)=1;
-    end;
-
-    tmp(tmp(end:-1:1,:)==1)=1;
-    tmp(tmp'==1)=1;
-
-    for j=1:16-rem_els
-        for i=1:32
-            ln = i+2*(j-1);
-            rw = 32-i+1;
-            if ln <= 32 && rw <=32
-                tmp(ln,rw)=1;
-            end;
-        end;
-    end;
-
-    tmp(tmp'==1)=1;
-    tmp(tmp(end:-1:1,:)==1)=1;
-    tmp(tmp(:,end:-1:1)==1)=1;
-
-    arr = arr.adaptArray(tmp, 90000, 0, 0);
-    
-    el_ratio = num2str(length(find(tmp~=0))/numel(tmp)*100,3);
-    
-    arr = arr.setComments(sprintf([num2str(rem_els) ' lines removed\n' ...
-        'Elements spacing: 0.84$\\lambda$\n\\# of elements: ' el_ratio '\\%%']));
-
-    % Plots
-    arr.plotAntArray();
-    arr = arr.genPattern(11000, 3000, 'XY', 30);
-    arr = arr.genPattern([], [], 'XY-BW');
-    arr = arr.E_strength(15000, 0, 0, 500);
-    arr = arr.genPattern([1 2:2:10]*1000, 3000, 'YZ', 30);
-    arr = arr.genPattern([1 2:2:10]*1000, [], 'YZ-BW');
-end;
+% for rem_els=0:15
+%     disp(['Squares 2, iteration ' num2str(rem_els-8) ' of ' num2str(15-8)]);
+%     % Init
+%     arr = AntArray(zeros(64), 60500, [], 0.84);
+%     arr = arr.setName(['sq2_' mat2str(rem_els)]);
+%     arr = arr.setMax('XY', 30);
+%     arr = arr.setMax('YZ', 30);
+%     arr = arr.setMax('E', 25);
+%     arr = arr.setMin('XY', -60);
+%     arr = arr.setMin('YZ', -60);
+%     arr = arr.setMin('E', -15);
+% 
+%     % Create elements' pattern
+%     tmp = zeros(64);
+%     for i=16:31-rem_els
+%         tmp(i, i:end-i+1)=1;
+%     end;
+% 
+%     tmp(tmp(end:-1:1,:)==1)=1;
+%     tmp(tmp'==1)=1;
+% 
+%     for j=1:16-rem_els
+%         for i=1:32
+%             ln = i+2*(j-1);
+%             rw = 32-i+1;
+%             if ln <= 32 && rw <=32
+%                 tmp(ln,rw)=1;
+%             end;
+%         end;
+%     end;
+% 
+%     tmp(tmp'==1)=1;
+%     tmp(tmp(end:-1:1,:)==1)=1;
+%     tmp(tmp(:,end:-1:1)==1)=1;
+% 
+%     arr = arr.adaptArray(tmp, 90000, 0, 0);
+%     
+%     el_ratio = num2str(length(find(tmp~=0))/numel(tmp)*100,3);
+%     
+%     arr = arr.setComments(sprintf([num2str(rem_els) ' lines removed\n' ...
+%         'Elements spacing: 0.84$\\lambda$\n\\# of elements: ' el_ratio '\\%%']));
+% 
+%     % Plots
+%     arr.plotAntArray();
+%     arr = arr.genPattern(11000, 3000, 'theta', 30, 0);
+%     arr = arr.genPattern([], [], 'theta-BW', [], 0);
+%     arr = arr.genPattern(11000, 3000, 'XY', 30);
+%     arr = arr.genPattern([], [], 'XY-BW');
+%     arr = arr.E_strength(15000, 0, 0, 500);
+%     arr = arr.genPattern([1 2:2:10]*1000, 3000, 'YZ', 30);
+%     arr = arr.genPattern([1 2:2:10]*1000, [], 'YZ-BW');
+% end;
 
 % Triangles
 % for rem_els=0:15
@@ -177,6 +183,8 @@ end;
 % 
 %     % Plots
 %     arr.plotAntArray();
+%     arr = arr.genPattern(11000, 3000, 'theta', 30, 0);
+%     arr = arr.genPattern([], [], 'theta-BW', [], 0);
 %     arr = arr.genPattern(11000, 3000, 'XY', 30);
 %     arr = arr.genPattern([], [], 'XY-BW');
 %     arr = arr.E_strength(15000, 0, 0, 500);
@@ -226,6 +234,8 @@ end;
 % 
 %     % Plots
 %     arr.plotAntArray();
+%     arr = arr.genPattern(11000, 3000, 'theta', 30, 0);
+%     arr = arr.genPattern([], [], 'theta-BW', [], 0);
 %     arr = arr.genPattern(11000, 3000, 'XY', 30);
 %     arr = arr.genPattern([], [], 'XY-BW');
 %     arr = arr.E_strength(15000, 0, 0, 500);
