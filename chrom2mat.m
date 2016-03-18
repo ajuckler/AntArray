772b14986e0721512898cbae0b2fa02ebc190d89
ajuckler <Antoine.Juckler@UGent.be>
2016-03-18 15:16:43 +0900

    Support of non-quantized matrices. mat2chrom added

--- chrom2mat.m
+++ chrom2mat.m
@@ -1,6 +1,16 @@
-function C = chrom2mat(chrom)
+function C = chrom2mat(chrom, quant)
 % Transforms a chromosome to its matrix representation
+%
+% INPUT:
+%   chrom:  chromosome to be reshaped
+%   quant:  (optional) is the end matrix quantized?
+%
 
+if nargin < 2
+    quant = 1;
+end;
+
+% Check chromosome length
 chrom_sz = sqrt(length(chrom));
 if abs(round(chrom_sz) - chrom_sz) > 10^-6
     error 'Invalid chromosome length';
@@ -8,12 +18,19 @@ else
     chrom_sz = round(chrom_sz);
 end;
 
-C = zeros(chrom_sz*4);
-chrom = repmat(chrom, 2, 1);
-chrom = reshape(chrom, chrom_sz*2, chrom_sz);
+if ~quant
+    C = zeros(chrom_sz*2);
+    C(1:end/2, 1:end/2) = reshape(chrom, chrom_sz, chrom_sz);
+else
+    C = zeros(chrom_sz*4);
+    chrom = repmat(chrom, 2, 1);
+    chrom = reshape(chrom, chrom_sz*2, chrom_sz);
 
-C(1:end/2, 1:2:end/2) = chrom;
-C(1:end/2, 2:2:end/2) = chrom;
+    C(1:end/2, 1:2:end/2) = chrom;
+    C(1:end/2, 2:2:end/2) = chrom;
+end;
+
+% Symmetries
 C(C(end:-1:1,:)==1) = 1;
 C(C(:,end:-1:1)==1) = 1;
 
---
+++ mat2chrom.m
@@ -0,0 +1,30 @@
+function chrom = mat2chrom(mat)
+% Transforms a matrix into a chromosome
+%
+% INPUT:
+%   mat:    matrix to be coded
+%
+
+% Check that matrix is square
+if size(mat, 1) ~= size(mat, 2)
+    error 'mat is not a square matrix';
+end;
+
+% Check that matrix is symmetric
+if ~isempty(mat(mat ~= mat(end:-1:1,:))) || ...
+   ~isempty(mat(mat ~= mat(:,end:-1:1)))
+    error 'mat is not symmetric';
+else
+    mat = mat(1:end/2, 1:end/2);
+end;
+
+% Check whether matrix is quantized
+if sum(sum(mat(1:2:end,:) ~= mat(2:2:end,:))) || ...
+    sum(sum(mat(:,1:2:end) ~= mat(:,2:2:end)))
+    chrom = reshape(mat, 1, numel(mat));
+else
+    chrom = reshape(mat(:,1:2:end), 1, numel(mat)/2);
+    chrom = chrom(1:2:end);
+end;
+
+end
