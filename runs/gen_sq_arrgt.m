1d3382159e839e797f3c28ab3672817cc616392c
ajuckler <Antoine.Juckler@UGent.be>
2016-05-20 19:27:36 +0900

    Functions to generate square and triangle patterns

---
+++ runs/gen_sq_arrgt.m
@@ -0,0 +1,46 @@
+%GEN_SQ_ARRGT generates the double square arrangement with specified
+%dimension
+%
+%   ARR = GEN_SQ_ARRGT(DIM)
+%   INPUT:
+%       DIM:    dimension of the array
+%       REM:    (optional) number of lines to remove [default = 0]
+%   OUTPUT:
+%       ARR:    the generated binary array
+%
+
+% Copyright 2016, Antoine Juckler. All rights reserved
+
+function arr = gen_sq_arrgt(dim, rem_els)
+    if nargin < 2
+        rem_els = 0;
+    else
+        rem_els = abs(round(rem_els));
+    end;
+    
+    if mod(dim,2) ~= 0
+        error 'DIM should be an even number';
+    end;
+    
+    arr = zeros(dim);
+    for i=round(dim/4):dim/2-1-rem_els
+        arr(i, i:end-i+1)=1;
+    end;
+
+    arr(arr(end:-1:1,:)==1)=1;
+    arr(arr'==1)=1;
+
+    for j=1:round(dim/4)-rem_els
+        for i=1:dim/2
+            ln = i+2*(j-1);
+            rw = dim/2-i+1;
+            if ln <= dim/2 && rw <= dim/2
+                arr(ln,rw)=1;
+            end;
+        end;
+    end;
+
+    arr(arr'==1)=1;
+    arr(arr(end:-1:1,:)==1)=1;
+    arr(arr(:,end:-1:1)==1)=1;
+end
---
+++ runs/gen_tr_arrgt.m
@@ -0,0 +1,60 @@
+%GEN_TR_ARRGT generates the two/four triangle arrangement with specified
+%dimension
+%
+%   ARR = GEN_TR_ARRGT(DIM, MODE, REM)
+%   INPUT:
+%       DIM:    dimension of the array
+%       MODE:   number of triangles (2 or 4)
+%       REM:    (optional) number of lines to remove [default = 0]
+%   OUTPUT:
+%       ARR:    the generated binary array
+%
+
+% Copyright 2016, Antoine Juckler. All rights reserved
+
+function arr = gen_tr_arrgt(dim, mode, rem)
+    if nargin < 3
+        rem = 0;
+    else
+        rem = abs(round(rem));
+    end;
+    if mode ~= 2 && mode ~= 4
+        error 'Invalid MODE argument';
+    end;
+    
+    b_fl = dim/(sqrt(3)+1/sqrt(3))*2;
+    b = 2*round(b_fl/2);
+    h = round(b_fl/2*sqrt(3));
+    
+    rem_els = max(1, ceil(h/3 - rem));
+
+    arr = zeros(32);
+    mask = arr;
+    for i=1:h
+        x = dim/2 - round((i-1)*tan(pi/6));
+        arr(i, x) = 1;
+        mask(i, x:end) = 1;
+    end
+    mask(h+1:end,:) = 0;
+    for k=1:rem_els
+        arr(h-k+1, dim/2:-1:dim/2-b/2+1) = 1;
+    end;
+
+    k_max = ceil(rem_els*tan(pi/3));
+    for k=2:min(k_max, dim-h)
+        for i=1:h
+            x = dim/2 - round((i-1)*tan(pi/6));
+            arr(i+k-1, x) = 1;
+        end;
+    end;
+
+    arr(mask == 0) = 0;
+
+    arr(arr(end:-1:1,:)==1)=1;
+    arr(arr(:,end:-1:1)==1)=1;
+    
+    if mode == 4
+        arr(arr'==1)=1;
+    end;
+
+end
