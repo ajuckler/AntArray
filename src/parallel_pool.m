453e52a3a788fb715fc62beaa4dfa31fe8a66e2b
ajuckler <Antoine.Juckler@UGent.be>
2016-07-04 12:05:18 +0900

    Added worker count functionality

--- src/parallel_pool.m
+++ src/parallel_pool.m
@@ -1,15 +1,21 @@
 %PARALLEL_POOL starts or stops parallel pool
 %
-%   [] = PARALLEL_POOL(MODE)
+%   RES = PARALLEL_POOL(MODE)
 %   INPUT:
 %       MODE:   'start' to start the pool
 %               'stop' to stop the pool
+%               'status' to get number of workers
 %               If the pool was already started/stopped, nothing will
 %               happen
+%   OUTPUT:
+%       RES:    depends on the inputted mode
+%               if mode is 'start' or 'stop':   0 for success
+%                                               -1 for error
+%               if mode is 'status':            number of workers
 
 % Copyright 2016, Antoine Juckler. All rights reserved
 
-function parallel_pool(mode)
+function res = parallel_pool(mode)
     persistent poolobj;
 
     if strcmp(mode, 'start')
@@ -22,6 +28,7 @@ function parallel_pool(mode)
                 poolobj = parpool;
             end;
         end;
+        res = 0;
     elseif strcmp(mode, 'stop')
         if verLessThan('matlab','8.2') 
             if matlabpool('size')
@@ -33,8 +40,20 @@ function parallel_pool(mode)
                 poolobj = [];
             end;
         end;
+        res = 0;
+    elseif strcmp(mode, 'status')
+        if verLessThan('matlab', '8.2')
+            res = matlabpool('size');
+        else
+            if ~isempty(gcp('nocreate'))
+                res = poolobj.NumWorkers;
+            else
+                res = 0;
+            end;
+        end;
     else
         poolobj = [];
+        res = -1;
         error 'Invalid mode parameter';
     end;
 
